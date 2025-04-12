4adfb67f01fea416428b7fdbfcb3ddde54b2f528
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-04-12 11:17:50 +0700

    feat: update LoginView to use MUI Container and apply styles

--- src/views/login/LoginView.tsx
+++ src/views/login/LoginView.tsx
@@ -1,6 +1,9 @@
+import { Container } from "@mui/material";
+import "../../styles/login/loginPage.css";
+
 const LoginView = () => {
-  document.title = "Smart Login"; 
-  return <div>LoginView</div>;
+  document.title = "Smart Login";
+  return <Container className="fullView">LoginView</Container>;
 };
 
 export default LoginView;
