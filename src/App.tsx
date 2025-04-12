0bfd878bf71ea978abaf8d7aa99a3319f5d1381d
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-04-12 11:09:49 +0700

    feat: implement LoginView component and integrate it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Container } from "@mui/material";
 import "./styles/login/loginPage.css";
+import LoginView from "./views/login/LoginView";
 
 function App() {
   document.title = `Smart Login`;
-  return <Container className="fullView"></Container>;
+  return (
+    <Container className="fullView">
+      <LoginView />
+    </Container>
+  );
 }
 
 export default App;
