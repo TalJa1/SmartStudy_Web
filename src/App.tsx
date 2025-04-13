e0e54397821a53664df197ed6997e52d7c97cc5a
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-04-13 08:34:07 +0700

    feat: add HomeView component and integrate with routing; include react-pro-sidebar dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import LoginView from "./views/login/LoginView";
+import HomeView from "./views/home/HomeView";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginView />} />
+      <Route path="/home" element={<HomeView />} />
     </Routes>
   );
 }
